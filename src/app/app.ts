855d899bfdd5c7b438ee821a0e5dec685456f666
Md Nishat Miah <nishat00101@gmail.com>
2025-08-08 01:00:40 +0600

    Added Header and Home components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('to-do-app');
+  protected readonly title = signal('To Do App');
 }
---
+++ src/app/components/header/header.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {
+  title = signal('My first Angular App!');
+}
